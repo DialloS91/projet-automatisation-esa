9c581b9c1e27853943b8cacb814226190d8558ee
DIALLO <saliejallow1@gmail.com>
2021-06-03 12:03:18 +0200

    Add the US4-CAS3 test

--- src/main/java/pageObject/HomePage.java
+++ src/main/java/pageObject/HomePage.java
@@ -2,11 +2,12 @@ package pageObject;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class HomePage {
     WebDriver driver;
     By signinButtonSelector = By.cssSelector(".header_user_info");
+    By searchBar = By.id("search_query_top");
+    By searchSubmit = By.cssSelector("[name = 'submit_search']");
 
 
     public HomePage(WebDriver driver) {
@@ -17,6 +18,13 @@ public class HomePage {
         System.out.println("Open Authentication page");
         driver.findElement(signinButtonSelector).click();
         return new AuthenticationPage(driver);
+    }
+
+    public SearchPage searchBarArticle(String article) {
+        System.out.println("Search an Article and submit");
+        driver.findElement(searchBar).sendKeys(article);
+        driver.findElement(searchSubmit).click();
+        return new SearchPage(driver);
     }
 
 
